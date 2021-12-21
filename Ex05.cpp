e0c996afcad26c1c73eb99a30a8de4cd90def57c
foptun <oixuier.toppfun@gmail.com>
2021-12-21 18:24:06 +0700

    Create Ex05.cpp

---
+++ Ex05.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int r = 0;
+    printf("Enter number of integers in a row (R): ");
+    scanf("%d", &r);
+    
+    int n = 0;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n ; i++) {
+        printf("%d ", i);
+        if (i % r == 0) {
+            printf("\n");
+        }
+    }
+    
+    
+    
+    return 0;
+}
